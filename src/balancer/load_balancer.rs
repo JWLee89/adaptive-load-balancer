acc6842500f2c67d89caed3177b42589066ef5f3
Jay Lee <jay.lee@lunit.io>
2024-09-18 22:47:01 +0900

    feat: add round robin algorithm

--- src/balancer/load_balancer.rs
+++ src/balancer/load_balancer.rs
@@ -1,6 +1,5 @@
-use color_eyre::{eyre::eyre, Result};
-use std::fmt::Debug;
-use std::{collections::HashMap, sync::Arc};
+use color_eyre::Result;
+use std::{fmt::Debug, sync::Arc};
 use tokio::sync::RwLock;
 
 use super::server::Server;
@@ -8,90 +7,24 @@ use super::server::Server;
 pub type LoadBalancerType = Arc<RwLock<dyn LoadBalancer<Server> + Send + Sync>>;
 
 #[async_trait::async_trait]
+pub trait LoadBalancerTemp<T> {}
+
+#[async_trait::async_trait]
 pub trait LoadBalancer<T>: Send
 where
     T: Debug + Send,
 {
+    /// Add a new server to the load balancer
     async fn add_server(&mut self, server: T) -> Result<()>;
+    // Maybe instead of string, make it a generic.
+    // This will make things more complex, but will make interface more flexible
+
+    /// Remove server from the load balancer
     async fn remove_server(&mut self, key: &str) -> Result<()>;
+
+    /// Retrieve the next server we should be forwarding request to
     async fn next_server(&mut self) -> Result<T>;
+
+    /// Get the number of servers
     async fn server_count(&self) -> usize;
-    async fn update_server_load(&mut self, server: &T, delta: isize);
-}
-
-#[derive(Debug, PartialEq)]
-pub struct RoundRobinLoadBalancer<T>
-where
-    T: Debug + Send + Clone,
-{
-    servers: HashMap<String, T>,
-    current_index: usize,
-}
-
-impl<T> Default for RoundRobinLoadBalancer<T>
-where
-    T: Debug + PartialEq + Send + Clone,
-{
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<T> RoundRobinLoadBalancer<T>
-where
-    T: Debug + PartialEq + Send + Clone,
-{
-    pub fn new() -> Self {
-        Self {
-            servers: HashMap::new(),
-            current_index: 0,
-        }
-    }
-}
-
-#[async_trait::async_trait]
-impl<T> LoadBalancer<T> for RoundRobinLoadBalancer<T>
-where
-    T: Debug + Clone + PartialEq + Send + Sync,
-{
-    async fn add_server(&mut self, server: T) -> Result<()> {
-        let key = self.servers.keys().len().to_string();
-        if self.servers.contains_key(&key) {
-            return Err(eyre!("Server with key {:?} already exists", key));
-        }
-        self.servers.insert(key, server);
-        Ok(())
-    }
-
-    async fn remove_server(&mut self, key: &str) -> Result<()> {
-        if self.servers.remove(key).is_some() {
-            if self.current_index >= self.servers.keys().len() {
-                self.current_index = 0;
-            }
-            Ok(())
-        } else {
-            Err(eyre!("Server not found"))
-        }
-    }
-
-    async fn next_server(&mut self) -> Result<T> {
-        if self.servers.is_empty() {
-            return Err(eyre!("List of target servers is empty"));
-        }
-
-        let key = self.current_index.to_string();
-        let server = self.servers.get(&key).cloned();
-
-        self.current_index = (self.current_index + 1) % self.servers.keys().len();
-
-        server.ok_or_else(|| eyre!("Server not found"))
-    }
-
-    async fn server_count(&self) -> usize {
-        self.servers.len()
-    }
-
-    async fn update_server_load(&mut self, _server: &T, _delta: isize) {
-        // Round Robin does not track server loads.
-    }
 }
